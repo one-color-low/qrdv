738fcf7a5bda5cdb0681634d254e00ecb4276b6d
one-color-low <gakki.uec15@gmail.com>
2022-09-15 00:40:08 +0900

    Initial Commit

---
+++ qrdb_v0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  qrdb_v0
+//
+//  Created by Yuta on 2022/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qrdb_v0/qrdb_v0App.swift
@@ -0,0 +1,17 @@
+//
+//  qrdb_v0App.swift
+//  qrdb_v0
+//
+//  Created by Yuta on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct qrdb_v0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
